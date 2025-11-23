743267573e739ea1a098d47d5de961937afca9c0
HenriqueOliveira29 <henrique.olive29@gmail.com>
2025-11-23 16:53:46 +0000

    add attributes to the DTO, i will update the others on developing new features

--- ReadingCommunityAPI.Application/Dtos/Author/AuthorDetailDTO.cs
+++ ReadingCommunityAPI.Application/Dtos/Author/AuthorDetailDTO.cs
@@ -6,5 +6,10 @@ public class AuthorDetailDTO
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Biography { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public DateTime? DeathDate { get; set; }
+    public string Nationality { get; set; }
+    public string ProfileImageUrl { get; set; }
     public List<BookListDTO> books{ get; set; }
 }
